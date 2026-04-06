d5f0c744beeee2e44aa0576ddcc74de62a76f7e1
KOSTEN19 <64401186+KOSTEN19@users.noreply.github.com>
2026-04-06 12:48:07 +0300

    Add files via upload

--- crawler-service/src/main/java/com/parsesite/crawler/CrawlerApplication.java
+++ crawler-service/src/main/java/com/parsesite/crawler/CrawlerApplication.java
@@ -2,8 +2,12 @@ package com.parsesite.crawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.parsesite")
+@EntityScan(basePackages = "com.parsesite.common.entity")
+@EnableJpaRepositories(basePackages = "com.parsesite.crawler.repository")
 public class CrawlerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CrawlerApplication.class, args);
